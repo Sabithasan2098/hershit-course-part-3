ac5ad8db721e061826cfe065d96659b43528acbb
Sabithasan2098 <sabithasan2098@gmail.com>
2023-12-28 09:22:07 +0600

    setTimeOut

---
+++ 1.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+console.log("Good morning");
+const name = setTimeout(() => {
+  console.log("my name is saful");
+}, 2000);
+const name2 = setTimeout(() => {
+  console.log("my name is sabit");
+}, 1000);
+const name3 = setTimeout(() => {
+  console.log("my name is Goda");
+}, 5000);
